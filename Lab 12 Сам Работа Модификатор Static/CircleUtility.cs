dba00ac82dd526a64b3e4d4fd95dde7d4967d91e
Maris Shapiro <19maris72@gmail.com>
2023-02-01 11:46:54 +0300

    Добавьте файлы проекта.

---
+++ Lab 12 Сам Работа Модификатор Static/CircleUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12_Сам_Работа_Модификатор_Static
+{
+    public static class Circle
+    {
+            public static double Length(double radius)
+            {
+                return 2 * Math.PI * radius;
+            }
+            public static double GetSquare(double radius)
+            {
+                return Math.PI * radius * radius;
+            }
+            public static bool ContainsPoint(double x, double y, double x0, double y0, double radius)
+            {
+                double distance = Math.Sqrt(Math.Pow(x - x0, 2) + Math.Pow(y - y0, 2));
+                return distance <= radius;
+            }
+    }
+}
---
+++ Lab 12 Сам Работа Модификатор Static/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12_Сам_Работа_Модификатор_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            double squae = Circle.GetSquare(radius);
+            double perimeter = Circle.Length(radius);
+            Console.WriteLine($"{squae} \n{perimeter}");
+            Console.ReadKey();
+
+
+            if (Circle.ContainsPoint(10, 10, 5, 5, 20))
+            {
+                Console.WriteLine("точки X и Y находятся внутри окружности");
+            }
+            else
+            {
+                Console.WriteLine("точки X и Y находятся вне окружности");
+            }
+            Console.ReadLine();
+        }
+    }
+}
